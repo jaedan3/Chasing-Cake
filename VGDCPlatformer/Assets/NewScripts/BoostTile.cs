80a1587f3a3176ac7cc986a85ba492c30c4f23e1
Joon <j00nj0321@gmail.com>
2018-10-27 13:51:38 -0700

    edited player script and added boost tile

---
+++ VGDCPlatformer/Assets/NewScripts/BoostTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTile : MonoBehaviour {
+    PlayerMovementBeginner player;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Speedup");
+            player.setSpeedMultiplier(2);
+        }
+    }
+}
