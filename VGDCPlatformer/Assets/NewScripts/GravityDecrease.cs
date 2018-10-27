c3b90bca64638b25c72c99777ecfad31ceed8c73
jaedan3 <klimekk@uci.edu>
2018-10-27 15:18:12 -0700

    gravity scripts

---
+++ VGDCPlatformer/Assets/NewScripts/GravityDecrease.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityDecrease : MonoBehaviour {
+    Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("GravityDecreasing");
+            player.gravityScale = 0.7f;
+        }
+    }
+}
---
+++ VGDCPlatformer/Assets/NewScripts/GravityIncrease.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityIncrease : MonoBehaviour {
+    Rigidbody2D player;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("GravityIncreasing");
+            player.gravityScale = 2;
+        }
+    }
+// Resets the gravityScale back to 1
+/*
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            player.gravityScale = 1;
+
+        }
+    }
+*/
+}
