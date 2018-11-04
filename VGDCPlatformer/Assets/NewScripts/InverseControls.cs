f6bbab51f7a98e0418c2c077043c2f27e679d86e
Michael Dela Cruz <michajd1@uci.edu>
2018-11-04 14:32:42 -0800

    Inverse

---
+++ VGDCPlatformer/Assets/NewScripts/InverseControls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseControls : MonoBehaviour {
+    PlayerMovementBeginner player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(Wait());   
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        StartCoroutine(Wait2());
+        yield return new WaitForSecondsRealtime(5);
+        player.changeHInput("Horizontal");
+    }
+
+    IEnumerator Wait2()
+    {
+        player.changeHInput("Horizontal");
+        yield return new WaitForSecondsRealtime(1);
+        player.changeHInput("Horizontal2");
+    }
+}
