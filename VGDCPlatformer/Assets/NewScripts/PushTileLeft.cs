7b87323a70ac24b03244f35a407577266ed356ab
Annie <anniezhao107@gmail.com>
2018-11-04 14:18:40 -0800

    made push tile up, but push tile left not working

--- VGDCPlatformer/Assets/NewScripts/PushTileLeft.cs
+++ VGDCPlatformer/Assets/NewScripts/PushTileLeft.cs
@@ -5,6 +5,7 @@ using System.Threading;
 
 public class PushTileLeft : MonoBehaviour {
     PlayerMovementBeginner player;
+    public float push;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
@@ -15,29 +16,31 @@ public class PushTileLeft : MonoBehaviour {
 		
 	}
 
-    /*IEnumerator waittest()
+    IEnumerator waittest()
     {
-        Debug.Log("wait");
-        yield return new WaitForSecondsRealtime(3);
-        Debug.Log("wait 2");
-    }*/
+        yield return new WaitForSeconds(.1f);
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            //StartCoroutine(waittest());
-            player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
-            player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
-            player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+            StartCoroutine(waittest());
+            GameObject obj = collision.gameObject;
+            Rigidbody2D rigid = obj.GetComponent<Rigidbody2D>();
+            Debug.Log("check");
+            rigid.velocity = new Vector2(-push,0);
+            //player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+            //player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+            //player.changePosition(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
         }
     }
-
+    /*
     public void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
 
         }
-    }
+    }*/
 }
---
+++ VGDCPlatformer/Assets/NewScripts/PushTileUp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class PushTileUp : MonoBehaviour
+{
+    //PlayerMovementBeginner player;
+    public float push;
+    // Use this for initialization
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator waittest()
+    {
+        yield return new WaitForSeconds(.1f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(waittest());
+            GameObject obj = collision.gameObject;
+            Rigidbody2D rigid = obj.GetComponent<Rigidbody2D>();
+            Debug.Log("check");
+            rigid.velocity = new Vector2(0,push);
+        }
+    }
+    /*
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+
+        }
+    }*/
+}
