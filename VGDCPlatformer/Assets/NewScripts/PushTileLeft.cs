2e0e44dba34a93033cd184f196bf8259c7e0f35e
Annie <anniezhao107@gmail.com>
2018-11-10 16:32:08 -0800

    new sprites for push tiles, changed left and right push tile script, removed top grass layer of tiles

--- VGDCPlatformer/Assets/NewScripts/PushTileLeft.cs
+++ VGDCPlatformer/Assets/NewScripts/PushTileLeft.cs
@@ -5,7 +5,7 @@ using System.Threading;
 
 public class PushTileLeft : MonoBehaviour {
     PlayerMovementBeginner player;
-    public float push;
+    public float newPos;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
@@ -16,22 +16,23 @@ public class PushTileLeft : MonoBehaviour {
 		
 	}
 
-    IEnumerator waittest()
+    /*IEnumerator waittest()
     {
         yield return new WaitForSeconds(.1f);
-    }
+    }*/
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            player.changePosition(gameObject.transform.position.x - newPos, player.transform.position.y, player.transform.position.z);
             /*StartCoroutine(waittest());
             GameObject obj = collision.gameObject;
             Rigidbody2D rigid = obj.GetComponent<Rigidbody2D>();
             Debug.Log("check");
             rigid.velocity = new Vector2(-push,0);*/
-            Vector3 pushLeft = new Vector3(-push, 0, 0);
-            player.transform.Translate(pushLeft * Time.deltaTime);
+            //Vector3 pushLeft = new Vector3(-push, 0, 0);
+            //player.transform.Translate(pushLeft * Time.deltaTime);
         }
     }
     
--- VGDCPlatformer/Assets/NewScripts/PushTileRight.cs
+++ VGDCPlatformer/Assets/NewScripts/PushTileRight.cs
@@ -6,7 +6,7 @@ using System.Threading;
 public class PushTileRight : MonoBehaviour
 {
     PlayerMovementBeginner player;
-    public float push;
+    public float newPos;
     // Use this for initialization
     void Start()
     {
@@ -19,30 +19,12 @@ public class PushTileRight : MonoBehaviour
 
     }
 
-    IEnumerator waittest()
-    {
-        yield return new WaitForSeconds(.1f);
-    }
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            /*StartCoroutine(waittest());
-            GameObject obj = collision.gameObject;
-            Rigidbody2D rigid = obj.GetComponent<Rigidbody2D>();
-            Debug.Log("check");
-            rigid.velocity = new Vector2(-push,0);*/
-            Vector3 pushLeft = new Vector3(push, 0, 0);
-            player.transform.Translate(pushLeft * Time.deltaTime);
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            //Vector3 pushLeft = new Vector3(-20, 0, 0);
+            player.changePosition(gameObject.transform.position.x+newPos, player.transform.position.y, player.transform.position.z);
+            //Vector3 pushLeft = new Vector3(push, 0, 0);
             //player.transform.Translate(pushLeft * Time.deltaTime);
         }
     }
