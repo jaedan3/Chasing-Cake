b18c903104adf6f8cfb4933d303a3506d933e1b4
Joon <j00nj0321@gmail.com>
2018-11-30 22:22:17 -0800

    boost tile sprite and spikes added

---
+++ VGDCPlatformer/Assets/NewScripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+    private GameObject player;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.SetActive(false);
+        }
+    }
+}
