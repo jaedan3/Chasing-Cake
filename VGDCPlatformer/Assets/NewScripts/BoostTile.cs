2eb7efa1af138c15b439773847e3c222153a4599
Michael Dela Cruz <michajd1@uci.edu>
2018-12-02 16:28:15 -0800

    speed map

--- VGDCPlatformer/Assets/NewScripts/BoostTile.cs
+++ VGDCPlatformer/Assets/NewScripts/BoostTile.cs
@@ -8,11 +8,16 @@ public class BoostTile : MonoBehaviour {
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementBeginner>();
+        
+    }
+
+    private void Awake()
+    {
         UIShit = GameObject.Find("SpeedUI");
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
